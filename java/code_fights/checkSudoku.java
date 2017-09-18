d7af34beb07515392e6f171b36fbba1308787b07
Annie Steenson <anniesteenso@gmail.com>
2017-09-18 08:38:42 -0500

    Added coding work done with codefights

--- java/code_fights/checkSudoku.java
+++ java/code_fights/checkSudoku.java
@@ -1,5 +1,52 @@
 package code_fights;
 
 public class checkSudoku {
+    boolean sudoku2(char[][] grid) {
+        if (grid == null || grid.length != 9 || grid[0].length != 9)
+            return false;
 
+        for (int i = 0; i < 9; i++) {
+            boolean[] rows = new boolean[9];
+            boolean[] cols = new boolean[9];
+
+            for (int j = 0; j < 9; j++) {
+                char curr = grid[i][j];
+
+                if (curr != '.') {
+                    System.out.println(curr);
+                    System.out.println(Character.getNumericValue(curr) - 1);
+
+                    if (rows[Character.getNumericValue(curr) - 1])
+                        return false;
+                    rows[Character.getNumericValue(curr) - 1] = true;
+                }
+
+                curr = grid[j][i];
+
+                if (curr != '.') {
+                    if (cols[Character.getNumericValue(curr) - 1])
+                        return false;
+                    cols[Character.getNumericValue(curr) - 1] = true;
+                }
+            }
+        }
+
+        for (int block = 0; block < 9; block++) {
+            boolean[] num = new boolean[9];
+
+            for (int i = (block/3) * 3; i < ((block/3) * 3) + 3; i++) {
+                for (int j = (block % 3) * 3; j < ((block % 3) * 3) + 3; j++) {
+                    char curr = grid[i][j];
+
+                    if (curr != '.') {
+                        if (num[Character.getNumericValue(curr) - 1])
+                            return false;
+                        num[Character.getNumericValue(curr) - 1] = true;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 }
---
+++ java/code_fights/isCryptSolution.java
@@ -0,0 +1,46 @@
+package code_fights;
+
+import java.util.HashMap;
+
+public class isCryptSolution {
+
+    public static void main(String args[]) {
+
+    }
+
+    boolean isCryptSolution(String[] crypt, char[][] solution) {
+        HashMap<Character, Character> key = new HashMap<>();
+
+        for (int i = 0; i < solution.length; i++) {
+            key.put(solution[i][0], solution[i][1]);
+        }
+
+        int[] decrypted = new int[crypt.length];
+        for (int i = 0; i < crypt.length; i++) {
+            String decrypt = "";
+            boolean leadingZero = false;
+            for (int j = 0; j  < crypt[i].length(); j++) {
+                char keyInt = key.get(crypt[i].charAt(j));
+                if (j == 0 && keyInt == '0') {
+                    leadingZero = true;
+                }
+
+                decrypt += keyInt;
+            }
+
+            if (leadingZero && decrypt.length() > 1)
+                return false;
+
+            try {
+                System.out.println(decrypt);
+                decrypted[i] = Integer.parseInt(decrypt);
+            } catch (Exception e) {
+                System.out.println("Caught Exception");
+                return false;
+            }
+        }
+
+        return ((decrypted[0] + decrypted[1]) == decrypted[2]);
+    }
+
+}
