a9d3380a62f78a6ff9ad0ddf97d6336b1ed9f3a0
anniesteenson <anniesteenso@gmail.com>
2017-10-14 13:19:34 -0500

    Solved findMaxSubarray

--- javascript/leetCode/maxSubarray.js
+++ javascript/leetCode/maxSubarray.js
@@ -1,45 +1,30 @@
 
 function findMaxSubarray(nums) {
-    let n = nums.length;
-    let dp = [];
-    let max = Number.NEGATIVE_INFINITY;
-    let max_i = 0;
-    let max_j = 0;
+    let max = nums[0];
+    let curr_max = nums[0];
+    let start = 0;
+    let end = 0;
 
-    let contSum = Number.NEGATIVE_INFINITY;
-
-    for (let i = 0; i < n; i++) {
-        dp[i] = [];
-        dp[i][i] = nums[i];
-        setMax(dp[i][i], i, i);
-
-        if (contSum == Number.NEGATIVE_INFINITY)
-            contSum = dp[i][i];
-        else
-            contSum += dp[i][i];
-        dp[0][i] = contSum;
-        setMax(contSum, 0, i);
-
-    }
-
-    for (let i = n - 1; i > 1; i--) {
-        for (let j = n - 2; j >= 1; j--) {
-            setMax(dp[0][i] - Math.abs(dp[0][j-1]), i, j);
+    for (let i = 1; i < nums.length; i++) {
+        if (curr_max + nums[i] < nums[i]) {
+            start = i;
+            end = i;
+            curr_max = nums[i];
         }
-    }
-
-    function setMax(val, i, j) {
-        if (max < val) {
-            max = val;
-            max_i = i;
-            max_j = j;
-            console.log("max "+max);
-            console.log("i "+max_i);
-            console.log("j "+max_j);
+        else {
+            curr_max += nums[i];
         }
+
+        if (curr_max > max) {
+            end = i;
+        }
+
+        max = Math.max(max, curr_max);
     }
 
-    return nums.slice(max_i, max_j+1);
+    console.log(start);
+    console.log(end);
+    return max;
 };
 
-console.log(findMaxSubarray([-2,-2,-3,0,-3,1,-3]));
+console.log(findMaxSubarray([-2,1,-3,4,-1,2,1,-5,4]));
